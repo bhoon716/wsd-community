523e7e46517ab61e0f9a0a43f3bcdb7615b163bc
박병훈 <leafloveu@naver.com>
2025-12-15 08:48:59 +0900

    feat/docs: Swagger OpenAPI 설정 추가

---
+++ src/main/java/wsd/community/config/OpenApiConfig.java
@@ -0,0 +1,25 @@
+package wsd.community.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("WSD-Community Open API Docs")
+                .description("WSD 동아리 웹사이트 api 문서입니다.")
+                .version("1.0.0");
+    }
+}
